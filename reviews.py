ad61690cd95834b13c360c25dc467f00642e0f12
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-02-19 14:52:23 -0300

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -4,8 +4,14 @@ class Reviews():
     def __init__(self, customer, restaurant, rating, comment):
         self.customer = customer
         self.restaurant = restaurant
-        self.rating = rating
+        self.set_rating(rating)
         self.comment = comment
+    
+    def set_rating(self, rating):
+        if 0 <= rating <= 5:
+            self.rating = rating
+        else:
+            raise ValueError("Rating must be between 0 and 5")
 
     def show_review(self):
         print(f'Customer: {self.customer}')
